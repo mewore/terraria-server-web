78f6a1568b97569cc3fbef745f7d420cb7f8b61d
mewore <46766863+mewore@users.noreply.github.com>
2021-03-03 23:36:42 +0200

    Fix the session interceptor
    1. It shouldn't add authorization data for requests that probably don't need authentication until it has been checked that the session is valid.
    2. More importantly, it shouldn't cause all requests to get sent twice.

--- frontend/src/app/core/incerceptors/session.interceptor.ts
+++ frontend/src/app/core/incerceptors/session.interceptor.ts
@@ -1,7 +1,8 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AuthenticationStateService } from '../services/authentication-state.service';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { AuthenticationStateService, SessionState } from '../services/authentication-state.service';
 
 @Injectable()
 export class SessionInterceptor implements HttpInterceptor {
@@ -9,7 +10,7 @@ export class SessionInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const authData = this.authenticationStateService.authData;
-        if (authData) {
+        if (authData && (this.isAuthenticated() || this.requestRequiresAuthentication(request))) {
             request = request.clone({
                 headers: request.headers.set('Authorization', authData),
             });
@@ -17,12 +18,23 @@ export class SessionInterceptor implements HttpInterceptor {
 
         const result = next.handle(request);
         if (authData) {
-            result.toPromise().catch((error) => {
-                if (error instanceof HttpErrorResponse && error.status === 401) {
-                    this.authenticationStateService.markAsUnsure();
-                }
-            });
+            return result.pipe(
+                catchError((error) => {
+                    if (error instanceof HttpErrorResponse && error.status === 401) {
+                        this.authenticationStateService.markAsUnsure();
+                    }
+                    return throwError(error);
+                })
+            );
         }
         return result;
+    }
+
+    private isAuthenticated(): boolean {
+        return this.authenticationStateService.sessionState === SessionState.AUTHENTICATED;
+    }
+
+    private requestRequiresAuthentication(request: HttpRequest<any>): boolean {
+        return request.method !== 'GET' && request.url !== '/auth/login' && request.url !== '/auth/signup';
     }
 }
