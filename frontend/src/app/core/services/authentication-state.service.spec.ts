2cb087858cb00fe46e78f12cc1949d5f1f81c885
mewore <46766863+mewore@users.noreply.github.com>
2021-07-30 21:56:53 +0300

    [#10] Implement authentication-state.service.spec.ts

--- frontend/src/app/core/services/authentication-state.service.spec.ts
+++ frontend/src/app/core/services/authentication-state.service.spec.ts
@@ -1,16 +1,80 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthenticationStateService } from './authentication-state.service';
+import { AuthenticationStateService, SessionState } from './authentication-state.service';
 
-xdescribe('AuthenticationStateService', () => {
+describe('AuthenticationStateService', () => {
     let service: AuthenticationStateService;
+
+    let unsureNotificationCount: number;
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
+
         service = TestBed.inject(AuthenticationStateService);
+
+        unsureNotificationCount = 0;
+        service.unsureObservable.subscribe({
+            next: () => unsureNotificationCount++,
+        });
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should have one unsure notification in the beginning', () => {
+        expect(unsureNotificationCount).toBe(1);
+    });
+
+    it('should be unauthenticated', () => {
+        expect(service.sessionState).toBe(SessionState.UNAUTHENTICATED);
+    });
+
+    describe('when the auth data is set to something', () => {
+        beforeEach(() => {
+            service.authData = 'authData';
+        });
+
+        it('should save the auth data', () => {
+            expect(service.authData).toBe('authData');
+        });
+
+        it('should become authenticated', () => {
+            expect(service.sessionState).toBe(SessionState.AUTHENTICATED);
+        });
+
+        describe('when the auth data is unset', () => {
+            beforeEach(() => {
+                service.authData = undefined;
+            });
+
+            it('should become unauthenticated', () => {
+                expect(service.sessionState).toBe(SessionState.UNAUTHENTICATED);
+            });
+        });
+
+        describe('when marked as unsure while authenticated', () => {
+            beforeEach(() => {
+                service.markAsUnsure();
+            });
+
+            it('should become unsure', () => {
+                expect(service.sessionState).toBe(SessionState.UNSURE);
+            });
+
+            it('should notify once about being unsure', () => {
+                expect(unsureNotificationCount).toBe(2);
+            });
+        });
+    });
+
+    describe('when marked as unsure while unauthenticated', () => {
+        beforeEach(() => {
+            service.markAsUnsure();
+        });
+
+        it('should stay unauthenticated', () => {
+            expect(service.sessionState).toBe(SessionState.UNAUTHENTICATED);
+        });
     });
 });
