6f58421df00dbbcb598fd690a6823684d305c8ca
mewore <46766863+mewore@users.noreply.github.com>
2021-07-30 16:48:53 +0300

    [#10] Complete app-component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -3,17 +3,19 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateServiceStub } from 'src/stub/translate.service.stub';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
-import { HeaderBarComponent } from './header/header-bar/header-bar.component';
 import { HeaderBarStubComponent } from './header/header-bar/header-bar.component.stub';
 
 describe('AppComponent', () => {
+    let translateService: TranslateServiceStub;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [RouterTestingModule],
             declarations: [AppComponent, HeaderBarStubComponent],
             providers: [{ provide: TranslateService, useClass: TranslateServiceStub }],
         }).compileComponents();
+
+        translateService = TestBed.inject(TranslateService);
     });
 
     async function instantiate(): Promise<AppComponent> {
@@ -27,7 +29,47 @@ describe('AppComponent', () => {
         expect(await instantiate()).toBeTruthy();
     });
 
+    it(`should add the en-US language to the list of languages`, async () => {
+        const addLangsSpy = spyOn(translateService, 'addLangs');
+
+        await instantiate();
+        expect(addLangsSpy).toHaveBeenCalledWith(['en-US']);
+    });
+
+    it(`should set the default language to 'en-US'`, async () => {
+        const setDefaultLangSpy = spyOn(translateService, 'setDefaultLang');
+
+        await instantiate();
+        expect(setDefaultLangSpy).toHaveBeenCalledWith('en-US');
+    });
+
     it(`should have the title 'Terraria Server Web'`, async () => {
         expect((await instantiate()).title).toEqual('Terraria Server Web');
+    });
+
+    describe('when the browser language is en-GB', () => {
+        beforeEach(() => {
+            spyOn(translateService, 'getBrowserLang').and.returnValue('en-GB');
+        });
+
+        it('should use the en-US language for i18n', async () => {
+            const useSpy = spyOn(translateService, 'use');
+
+            await instantiate();
+            expect(useSpy).toHaveBeenCalledWith('en-US');
+        });
+    });
+
+    describe('when the browser language is fr-FR', () => {
+        beforeEach(() => {
+            spyOn(translateService, 'getBrowserLang').and.returnValue('fr-FR');
+        });
+
+        it('should use the default (en-US) language for i18n', async () => {
+            const useSpy = spyOn(translateService, 'use');
+
+            await instantiate();
+            expect(useSpy).toHaveBeenCalledWith('en-US');
+        });
     });
 });
