04305d926674570497699c779e24188d0a3ae9e6
mewore <46766863+mewore@users.noreply.github.com>
2021-08-03 20:13:31 +0300

    [#10] Implement the unit test of simple-dialog.service.ts

--- frontend/src/app/core/simple-dialog/simple-dialog.service.spec.ts
+++ frontend/src/app/core/simple-dialog/simple-dialog.service.spec.ts
@@ -1,16 +1,55 @@
 import { TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogRefStub } from 'src/stubs/mat-dialog-ref.stub';
+import { MatDialogStub } from 'src/stubs/mat-dialog.stub';
+import { SimpleDialogComponent, SimpleDialogInput } from './simple-dialog.component';
 
 import { SimpleDialogService } from './simple-dialog.service';
 
-xdescribe('SimpleDialogService', () => {
+type DialogReturnType = string;
+
+describe('SimpleDialogService', () => {
     let service: SimpleDialogService;
 
+    let dialog: MatDialog;
+
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({ providers: [{ provide: MatDialog, useClass: MatDialogStub }] });
+        dialog = TestBed.inject(MatDialog);
+
         service = TestBed.inject(SimpleDialogService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('openDialog', () => {
+        let inputData: SimpleDialogInput<DialogReturnType>;
+
+        let dialogCloseValue: DialogReturnType;
+        let openSpy: jasmine.Spy;
+
+        beforeEach(() => {
+            inputData = {
+                titleKey: 'title',
+            } as SimpleDialogInput<DialogReturnType>;
+            dialogCloseValue = 'closeValue';
+            openSpy = spyOn(dialog, 'open').and.returnValue(MatDialogRefStub.withCloseValue(dialogCloseValue));
+        });
+
+        it('should be called with the correct parameters', async () => {
+            await service.openDialog(inputData);
+            expect(openSpy).toHaveBeenCalledWith(SimpleDialogComponent, {
+                data: inputData,
+                maxWidth: '30em',
+                minWidth: '20em',
+                width: '80%',
+            });
+        });
+
+        it('should return the correct value', async () => {
+            expect(await service.openDialog(inputData)).toBe(dialogCloseValue);
+        });
     });
 });
