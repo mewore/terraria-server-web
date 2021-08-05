8c456359402657c2cf232d6086e1781fed0d7bd3
mewore <46766863+mewore@users.noreply.github.com>
2021-08-05 17:32:00 +0300

    [#10] Implement the unit test of set-instance-mods-dialog.service.ts

--- frontend/src/app/terraria/set-instance-mods-dialog/set-instance-mods-dialog.service.spec.ts
+++ frontend/src/app/terraria/set-instance-mods-dialog/set-instance-mods-dialog.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MatDialog } from '@angular/material/dialog';
+import { TerrariaInstanceEntity } from 'src/generated/backend';
+import { MatDialogRefStub } from 'src/stubs/mat-dialog-ref.stub';
+import { MatDialogStub } from 'src/stubs/mat-dialog.stub';
+import { SetInstanceModsDialogComponent, SetInstanceModsDialogInput, SetInstanceModsDialogOutput } from './set-instance-mods-dialog.component';
 import { SetInstanceModsDialogService } from './set-instance-mods-dialog.service';
 
-xdescribe('SetInstanceModsDialogService', () => {
+describe('SetInstanceModsDialogService', () => {
     let service: SetInstanceModsDialogService;
 
+    let dialog: MatDialog;
+
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({ providers: [{ provide: MatDialog, useClass: MatDialogStub }] });
+        dialog = TestBed.inject(MatDialog);
+
         service = TestBed.inject(SetInstanceModsDialogService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('openDialog', () => {
+        let dialogCloseValue: TerrariaInstanceEntity;
+        let openSpy: jasmine.Spy;
+        let inputData: SetInstanceModsDialogInput;
+        let result: SetInstanceModsDialogOutput | undefined;
+
+        beforeEach(async () => {
+            dialogCloseValue = { id: 8 } as TerrariaInstanceEntity;
+            openSpy = spyOn(dialog, 'open').and.returnValue(MatDialogRefStub.withCloseValue(dialogCloseValue));
+            inputData = { id: 8 } as TerrariaInstanceEntity;
+            result = await service.openDialog(inputData);
+        });
+
+        it('should be called with the correct parameters', async () => {
+            expect(openSpy).toHaveBeenCalledWith(SetInstanceModsDialogComponent, {
+                data: inputData,
+                maxWidth: '50em',
+                minWidth: '20em',
+                width: '80%',
+            });
+        });
+
+        it('should return the correct value', async () => {
+            expect(result).toBe(dialogCloseValue);
+        });
     });
 });
