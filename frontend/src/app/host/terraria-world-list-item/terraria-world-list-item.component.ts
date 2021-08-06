4627f2bbaff41bff8e492a53390b52e844a257eb
mewore <46766863+mewore@users.noreply.github.com>
2021-08-06 20:42:26 +0300

    [#14] Ignore some Angular build warnings
    Sadly, I can neither reduce the bundle size nor get rid of the SockJS CommonJS dependency for now.

--- frontend/src/app/host/terraria-world-list-item/terraria-world-list-item.component.ts
+++ frontend/src/app/host/terraria-world-list-item/terraria-world-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TerrariaWorldEntity } from 'src/generated/backend';
 
 @Component({
